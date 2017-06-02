fab8a0e3e8aab09655b7536c784e2661141b3446
John Crisostomo <johncrisostomo@protonmail.ch>
2017-06-02 08:23:26 +0800

    tries animating the first card

--- src/Deck.js
+++ src/Deck.js
@@ -9,17 +9,33 @@ class Deck extends Component {
   constructor(props) {
     super(props);
 
+    const position = new Animated.ValueXY();
+
     const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
-      onPanResponderMove: (event, gesture) => {},
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({ x: gesture.dx , y: gesture.dy });
+      },
       onPanResponderRelease: () => {},
     });
 
-    this.state = { panResponder };
+    this.position = position;
+    this.state = { panResponder, position };
   }
 
   renderCards() {
-    return this.props.data.map(item => {
+    return this.props.data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            style={this.state.position.getLayout()}
+            {...this.state.panResponder.panHandlers}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      }
+
       return this.props.renderCard(item);
     });
   }
