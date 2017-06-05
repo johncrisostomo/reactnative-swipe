16c26de756c2f8a40f7a3e5e59dcf8fb00e71d34
John Crisostomo <johncrisostomo@protonmail.ch>
2017-06-05 08:00:57 +0800

    uses Animated.spring to animate the card back into initial pos  after release

--- src/Deck.js
+++ src/Deck.js
@@ -19,11 +19,18 @@ class Deck extends Component {
       onPanResponderMove: (event, gesture) => {
         position.setValue({ x: gesture.dx , y: gesture.dy });
       },
-      onPanResponderRelease: () => {},
+      onPanResponderRelease: () => {
+        this.resetPosition();
+      },
     });
 
-    this.position = position;
     this.state = { panResponder, position };
+  }
+
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: { x: 0, y: 0 }
+    }).start();
   }
 
   getCardStyle() {
