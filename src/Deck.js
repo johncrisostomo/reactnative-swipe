7ae85276b8b23328ea05829cbd2fa535e78eb72b
John Crisostomo <johncrisostomo@protonmail.ch>
2017-06-06 08:06:22 +0800

    implements swiping left / right and stubs the onSwipeComplete callback

--- src/Deck.js
+++ src/Deck.js
@@ -23,9 +23,9 @@ class Deck extends Component {
       },
       onPanResponderRelease: (event, gesture) => {
         if (gesture.dx > SWIPE_THRESHOLD) {
-          this.forceSwipeRight();
+          this.forceSwipe('right');
         } else if (gesture.dx < -SWIPE_THRESHOLD) {
-          console.log('swipe left')
+          this.forceSwipe('left');
         } else {
           this.resetPosition();
         }
@@ -35,11 +35,18 @@ class Deck extends Component {
     this.state = { panResponder, position };
   }
 
-  forceSwipeRight() {
+  forceSwipe(direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
     Animated.timing(this.state.position, {
-      toValue: { x: SCREEN_WIDTH, y: 0 },
+      toValue: { x, y: 0 },
       duration: SWIPE_OUT_DURATION,
-    }).start();
+    }).start(() => this.onSwipeComplete(direction));
+  }
+
+  onSwipeComplete(direction) {
+    const { onSwipeLeft, onSwipeRight } = this.props;
+
+    direction === 'right' ? onSwipeRight() : onSwipeLeft();
   }
 
   resetPosition() {
